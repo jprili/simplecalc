caeb1aee88caa3da4611db893c99688841560bf6
sbgum <rilijames@gmail.com>
2022-08-03 15:03:49 -0700

    added compute and graph templates

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sys
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Ui(QMainWindow):
 
@@ -12,12 +15,40 @@ class Ui(QMainWindow):
         self.pushRun.clicked.connect(self.runcalc)
 
     def runcalc(self):
+        """
+        public void runcalc
+
+        MODIFIES: self
+
+        EFFECTS: executes the corresponding methods depending on the
+                 current tab (either compute or graph) open.
+        """
         if self.tabWidget.currentIndex() == 0:
-            print(0)
+            self.compute()
         elif self.tabWidget.currentIndex() == 1:
-            print(1)
+            self.graph()
         else:
             print(-1)
+
+    def compute(self):
+        """
+        REQUIRES: valid compute text
+
+        EFFECTS: computes arithmetic
+
+        :return:
+        """
+        return None
+
+    def graph(self):
+        """
+        REQUIRES: valid equation/inequality
+
+        EFFECTS: displays a graph of the equation
+
+        :return:
+        """
+        return None
 
 
 calc = QApplication(sys.argv)
