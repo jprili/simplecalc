efa632d05f3335c459f56deec5916223c2e8ab53
sbgum <rilijames@gmail.com>
2022-08-03 13:18:38 -0700

    assign button

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import sys
 
 
 class Ui(QtWidgets.QMainWindow):
+
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('maincalc.ui', self)
         self.show()
 
+    def runCalc(self):
+        return None
+
+
 calc = QtWidgets.QApplication(sys.argv)
 mainWin = Ui()
+
 
 
 if __name__ == "__main__":
